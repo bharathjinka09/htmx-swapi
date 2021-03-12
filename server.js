b81a953215585cdcca0969c4b8790781a7a912b2
Rajasegar <rajasegar.c@gmail.com>
2021-03-12 11:58:58 +0000

    chore: render data from server
    1. replace node-fetch with axios
    2. replace ejs with pug

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 
 app.get('/', async (req, res) => {
-  res.render('index');
+  const response = await axios.get('https://swapi.dev/api/people');
+  console.log(response);
+  res.render('index', { people: response.data.results });
 });
 
 app.get('/api/people', async (req,res) => {
-  const response = await fetch('https://swapi.dev/api/people');
-  const data = await response.json();
+  const data = await axios.get('https://swapi.dev/api/people');
   res.json(data);
 });
 
 
 app.get('/api/planets', async (req,res) => {
-  const response = await fetch('https://swapi.dev/api/planets');
-  const data = await response.json();
+  const data = await axios.get('https://swapi.dev/api/planets');
   res.json(data);
 });
 
